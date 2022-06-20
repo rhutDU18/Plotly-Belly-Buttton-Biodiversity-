7fafeb0e9484dc77ebfc0080a3f6623245aa413f
Robert Hutchins <rhutchins747@gmail.com>
2022-06-20 16:15:06 -0600

    Plotly_Biodiversity

--- Belly_Diversity/BellyButton_bar_chart_starter_code.js
+++ Belly_Diversity/BellyButton_bar_chart_starter_code.js
@@ -119,3 +119,29 @@ function buildCharts(sample) {
     Plotly.newPlot('bar', barData, barLayout);  
   });
   }
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+      {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: "markers",
+        marker: {
+          size: sample_values,
+          color: otu_ids,
+          colorscale: "Earth"
+        }
+      }
+    ];
+    var bubbleData = [trace]
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: 'Bacteria Cultures Per Sample',
+      xaxis: {title: "OTU ID"},
+      showlegend: false 
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot('pie', bubbleData, bubbleLayout);
+  });
+}
